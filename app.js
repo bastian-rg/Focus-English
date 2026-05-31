89174f85a4e33a24370de818d1e3ec5cd215e98a
bastian-rg <bastianig19@gmail.com>
2026-05-31 01:54:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1462,11 +1462,13 @@ function agregarAudioEjemplos(html) {
         /<b>(.*?)<\/b>/g,
         (match, texto) =>
             `<b>${texto}</b>
-            <button 
+            <span 
                 onclick="event.stopPropagation(); playAudio('${texto.replace(/'/g,"\\'")}')"
-                style="margin-left:5px; cursor:pointer;">
-                🔊
-            </button>`
+                style="margin-left:6px; cursor:pointer; display:inline-flex; vertical-align:middle;">
+                <svg width="26" height="26" viewBox="0 0 24 24" fill="#808080">
+                    <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
+                </svg>
+            </span>`
     );
 }
 
